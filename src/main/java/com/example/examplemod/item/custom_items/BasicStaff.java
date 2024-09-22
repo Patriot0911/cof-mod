8d68890a1fa2ec55539a005e83a91947e5297b35
oles.potaskalov <alexandr.potaskalov@gmail.com>
2024-09-22 18:31:41 +0300

    inital

---
+++ src/main/java/com/example/examplemod/item/custom_items/BasicStaff.java
@@ -0,0 +1,30 @@
+package com.example.examplemod.item.custom_items;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class BasicStaff extends Item {
+    public BasicStaff(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        System.out.println(itemstack.getCount());
+        if (itemstack.isEdible()) {
+            if (player.canEat(itemstack.getFoodProperties(player).canAlwaysEat())) {
+                player.startUsingItem(hand);
+                return InteractionResultHolder.consume(itemstack);
+            } else {
+                return InteractionResultHolder.fail(itemstack);
+            }
+        } else {
+            return InteractionResultHolder.pass(player.getItemInHand(hand));
+        }
+    }
+}
---
+++ src/main/java/com/example/examplemod/item/custom_items_properties/BasicStaffProperties.java
@@ -0,0 +1,9 @@
+package com.example.examplemod.item.custom_items_properties;
+
+import net.minecraft.world.item.Item.Properties;
+
+public class BasicStaffProperties extends Properties {
+    public BasicStaffProperties() {
+        this.stacksTo(3);
+    }
+}
