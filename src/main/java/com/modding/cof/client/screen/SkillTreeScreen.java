ac276f003dc6a2c579ff196cc210b7baa6fc5400
Yaroslav Rusak <rusak.yaroslav@lll.kpi.ua>
2024-11-04 20:53:25 +0200

    buttons map

--- src/main/java/com/modding/cof/client/screen/SkillTreeScreen.java
+++ src/main/java/com/modding/cof/client/screen/SkillTreeScreen.java
@@ -1,5 +1,8 @@
 package com.modding.cof.client.screen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jetbrains.annotations.NotNull;
 
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -12,11 +15,13 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 public class SkillTreeScreen extends Screen {
-    private static final ResourceLocation BACKGROUND_TEXTURE =
-        new ResourceLocation("cof", "textures/gui/skill_tree_background.png");
+    private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation("cof", "textures/gui/skill_tree_background.png");
+    public static final int BACKGROUND_SIZE = 2048;
 
-    private float scrollX, scrollY;
+    private float scrollX = 0, scrollY = 0;
     private int screenWidth, screenHeight;
+    private float zoomLevel = 1.0f;
+    private List<SkillButton> skillButtons = new ArrayList<>();
 
     public SkillTreeScreen(ResourceLocation skillTreeId) {
         super(Component.literal("Skill Tree"));
@@ -26,6 +31,7 @@ public class SkillTreeScreen extends Screen {
     @Override
     public void init() {
         clearWidgets();
+        skillButtons.clear();
 
         screenWidth = 4 * this.width / 5;
         screenHeight = 4 * this.height / 5;
@@ -40,20 +46,57 @@ public class SkillTreeScreen extends Screen {
         addSkillButtons();
     }
 
-    private void addSkillButtons() { }
+    private void addSkillButtons() {
+        int buttonSize = 20;
+        int centerX = this.width / 2;
+        int centerY = this.height / 2;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int x = centerX + (i - 2) * 40 - buttonSize / 2;
+                int y = centerY + (j - 2) * 40 - buttonSize / 2;
+                SkillButton skillButton = new SkillButton(x, y, buttonSize, buttonSize, Component.literal("Skill"));
+                skillButtons.add(skillButton);
+                addRenderableWidget(skillButton);
+            }
+        }
+
+        if (skillButtons.size() > 1) {
+            for (int i = 0; i < skillButtons.size() - 1; i++) {
+                SkillButton button1 = skillButtons.get(i);
+                SkillButton button2 = skillButtons.get(i + 1);
+                button1.addConnection(button2);
+                button2.addConnection(button1);
+            }
+        }
+    }
 
     @Override
     public void render(@NotNull PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
         renderBackgroundTexture(poseStack);
 
-        renderSkills(poseStack, mouseX, mouseY, partialTick);
+        int bgLeft = (this.width - BACKGROUND_SIZE) / 2;
+        int bgTop = (this.height - BACKGROUND_SIZE) / 2;
+        poseStack.pushPose();
+        poseStack.translate(bgLeft, bgTop, 0);
+        RenderSystem.enableScissor(bgLeft, bgTop, BACKGROUND_SIZE, BACKGROUND_SIZE);
+
+        renderSkills(poseStack);
+
+        RenderSystem.disableScissor();
+        poseStack.popPose();
 
         super.render(poseStack, mouseX, mouseY, partialTick);
     }
 
     private void renderBackgroundTexture(PoseStack poseStack) {
-        int x = (this.width - screenWidth) / 2;
-        int y = (this.height - screenHeight) / 2;
+        int centerX = this.width / 2;
+        int centerY = this.height / 2;
+        int adjustedWidth = screenWidth;
+        int adjustedHeight = screenHeight;
+
+        int x = centerX - adjustedWidth / 2;
+        int y = centerY - adjustedHeight / 2;
 
         fill(poseStack, 0, 0, this.width, this.height, 0x88000000);
 
@@ -61,14 +104,37 @@ public class SkillTreeScreen extends Screen {
         blit(poseStack, x, y, 0, 0, screenWidth, screenHeight, screenWidth, screenHeight);
     }
 
-    private void renderSkills(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+    private void renderSkills(PoseStack poseStack) {
         poseStack.pushPose();
         poseStack.translate(scrollX, scrollY, 0);
+        poseStack.scale(zoomLevel, zoomLevel, 1.0f);
+
+        for (SkillButton skillButton : skillButtons) {
+            for (SkillButton connectedButton : skillButton.getConnectedButtons()) {
+                drawLineBetweenButtons(poseStack, skillButton, connectedButton, 0xFFFFFF);
+            }
+        }
+
         poseStack.popPose();
     }
 
+    private void drawLineBetweenButtons(PoseStack poseStack, SkillButton button1, SkillButton button2, int color) {
+        int startX = (int) ((button1.x + button1.getWidth() / 2) * zoomLevel);
+        int startY = (int) ((button1.y + button1.getHeight() / 2) * zoomLevel);
+        int endX = (int) ((button2.x + button2.getWidth() / 2) * zoomLevel);
+        int endY = (int) ((button2.y + button2.getHeight() / 2) * zoomLevel);
+
+        this.hLine(poseStack, startX, endX, startY, color);
+        this.vLine(poseStack, startX, startY, endY, color);
+    }
+
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        return super.mouseClicked(mouseX, mouseY, button);
+    public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
+        if (delta > 0) {
+            zoomLevel = Math.min(zoomLevel + 0.1f, 2.0f);
+        } else if (delta < 0) {
+            zoomLevel = Math.max(zoomLevel - 0.1f, 0.5f);
+        }
+        return true;
     }
 }
